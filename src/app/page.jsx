9256fe156757d864f92de7671fe980b5c3528511
samyak <samyakmhrzn9841@gmail.com>
2024-09-01 19:08:41 +0545

    layout and components

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,11 @@
 import Link from "next/link";
 import styles from "./homepage.module.css";
 
+
 export default function Home() {
-  return <div>
-    <Link href="/">Hello</Link>
-  </div>;
+  return (
+    <div>
+
+    </div>
+  );
 }
