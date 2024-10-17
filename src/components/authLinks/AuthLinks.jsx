61f4f62bb8cd7526ff44c9383f9689a7ae38203a
samyak <samyakmhrzn9841@gmail.com>
2024-10-17 16:46:52 +0545

    prisma + login

--- src/components/authLinks/AuthLinks.jsx
+++ src/components/authLinks/AuthLinks.jsx
@@ -3,16 +3,17 @@
 import React, { useState } from "react";
 import styles from "./authLinks.module.css";
 import Link from "next/link";
+import { signOut, useSession } from "next-auth/react";
 
 const AuthLinks = () => {
 
   const [open, setOpen] = useState(false);
 
-  const status = "not authenticated";
+  const { status } = useSession();
 
   return (
     <>
-      {status === "not authenticated" ? (
+      {status === "unauthenticated" ? (
         <Link href="/login" className={styles.link}>
           Login
         </Link>
@@ -21,7 +22,7 @@ const AuthLinks = () => {
           <Link href="/write" className={styles.link}>
             Write
           </Link>
-          <span className={styles.link}>Logout</span>
+          <span className={styles.link} onClick={signOut}>Logout</span>
         </>
       )}
 
@@ -35,7 +36,7 @@ const AuthLinks = () => {
           <Link href="/">Homepage</Link>
           <Link href="/">About</Link>
           <Link href="/">Contact</Link>
-          {status === "notauthenticated" ? (
+          {status === "unauthenticated" ? (
             <Link href="/login">Login</Link>
           ) : (
             <>
--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import styles from "./pagination.module.css"
+"use client";
 
-const Pagination = () => {
+import React from "react";
+import styles from "./pagination.module.css";
+import { useRouter } from "next/navigation";
+
+const Pagination = ({ page, hasPrev, hasNext }) => {
+  const router = useRouter();
+
   return (
     <div className={styles.container}>
-      {/* <button
+      <button
         className={styles.button}
         disabled={!hasPrev}
         onClick={() => router.push(`?page=${page - 1}`)}
@@ -17,9 +22,9 @@ const Pagination = () => {
         onClick={() => router.push(`?page=${page + 1}`)}
       >
         Next
-      </button> */}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Pagination
+export default Pagination;
