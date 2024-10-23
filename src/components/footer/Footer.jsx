83ca8229679f530fcf9ce5d6566a73830f59d9d2
samyak <samyakmhrzn9841@gmail.com>
2024-10-23 19:53:25 +0545

    posts date and blog page update

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -3,50 +3,42 @@ import styles from "./footer.module.css"
 import Image from 'next/image'
 import Link from 'next/link'
 
+import { FaGithub } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <div className={styles.container} id='footer_'>
       <div className={styles.info}>
         <div className={styles.logo}>
-          <Image src="/logo.png" alt="lama blog" width={50} height={50} />
-          <h1 className={styles.logoText}>Lamablog</h1>
+          <div className={styles.profile_cont}>
+            <Image src="/profile.jpg" alt="profile pic" fill objectFit='cover'/>
+          </div>
+          <h1 className={styles.logoText}>Samyak</h1>
         </div>
         <p className={styles.desc}>
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim
-          necessitatibus similique aspernatur obcaecati veritatis. Aperiam cum
-          porro sequi, totam minima consequuntur, aspernatur deleniti vero
-          repellendus dorales.
+          A passionate learner / computer engineer / student / web developer <br/>
+          Copyright © 2023 Samyak. All Rights Reserved.
         </p>
         <div className={styles.icons}>
-          <Image src="/facebook.png" alt="" width={18} height={18} />
-          <Image src="/instagram.png" alt="" width={18} height={18} />
-          <Image src="/mail.png" alt="" width={18} height={18} />
-          <Image src="/youtube.png" alt="" width={18} height={18} />
+        <a href="https://github.com/samyak0-0/" target="_blank">
+          <FaGithub size={18} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/samyak-maharjan-767909293/"
+          target="_blank"
+        >
+          <FaLinkedin size={18} />
+        </a>
+        <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+          <Image src="/youtube.png" alt="youtube" width={18} height={18} />
+        </a>
+        <a href="mailto:samyakmhrzn9841@gmail.com" target="_blank">
+          <Image src="/mail.png" alt="mail" width={18} height={18} />
+        </a>
         </div>
       </div>
-      <div className={styles.links}>
-        <div className={styles.list}>
-          <span className={styles.listTitle}>Links</span>
-          <Link href="/">Homepage</Link>
-          <Link href="/">Blog</Link>
-          <Link href="/">About</Link>
-          <Link href="/">Contact</Link>
-        </div>
-        <div className={styles.list}>
-          <span className={styles.listTitle}>Tags</span>
-          <Link href="/">Style</Link>
-          <Link href="/">Fashion</Link>
-          <Link href="/">Coding</Link>
-          <Link href="/">Travel</Link>
-        </div>
-        <div className={styles.list}>
-          <span className={styles.listTitle}>Social</span>
-          <Link href="/">Facebook</Link>
-          <Link href="/">Instagram</Link>
-          <Link href="/">E-mail</Link>
-          <Link href="/">Youtube</Link>
-        </div>
-      </div>
+      
     </div>
   )
 }
--- src/components/menuCategories/MenuCategories.jsx
+++ src/components/menuCategories/MenuCategories.jsx
@@ -19,8 +19,6 @@ const MenuCategories = async () => {
 
   const data = await getData();
 
-  // const colorr = `$`;
-
   return (
       <div className={styles.categoryList}>
         {data?.map((item) => (
@@ -36,33 +34,5 @@ const MenuCategories = async () => {
       </div>
   );
 };
-
-// const MenuCategories = () => {
-//   return (
-//     <div className={styles.categoryList}>
-//       <Link
-//         href="/blog?cat=style"
-//         className={`${styles.categoryItem} ${styles.style}`}
-//       >
-//         Style
-//       </Link>
-//       <Link href="/blog?cat=fashion" className={`${styles.categoryItem} ${styles.fashion}`}>
-//         Fashion
-//       </Link>
-//       <Link href="/blog?cat=food" className={`${styles.categoryItem} ${styles.food}`}>
-//         Food
-//       </Link>
-//       <Link href="/blog?cat=travel" className={`${styles.categoryItem} ${styles.travel}`}>
-//         Travel
-//       </Link>
-//       <Link href="/blog?cat=culture" className={`${styles.categoryItem} ${styles.culture}`}>
-//         Culture
-//       </Link>
-//       <Link href="/blog?cat=coding" className={`${styles.categoryItem} ${styles.coding}`}>
-//         Coding
-//       </Link>
-//     </div>
-//   );
-// };
 
 export default MenuCategories;
